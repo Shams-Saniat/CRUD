451c6d01c7529e433956ba61a0c872c4764363b2
shamssaniat <shams.saniat@northsouth.edu>
2025-04-18 22:20:48 +0600

    Configure React-Router-Dom for Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import AddUser from "./adduser/AddUser"
 import User from './getuser/User';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />,
+    },
+    {
+      path:"/add",
+      element: <AddUser />
+    },
+  ]);
   return (
-    <div className="App">
-      <User />
+    <div className="App" >
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
